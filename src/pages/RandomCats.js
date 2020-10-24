595d6b8d5aa21a2455e0e6486cc0f3264bfefead
Anton Martynov <nocturne6667@gmail.com>
2020-10-24 03:13:44 +0300

    margin fix randomcats page

--- src/pages/RandomCats.js
+++ src/pages/RandomCats.js
@@ -7,7 +7,7 @@ const RandomCats = ({ cats }) => {
       <h2 className="text-left font-bold leading-snug sm:leading-10  text-4xl sm:text-5xl ">
         10 Random Cat Breeds
       </h2>
-      <div className="mt-7 sm:mt-10 md:mt-14">
+      <div className="my-7 sm:mt-10 md:mt-14 ">
         {cats.map((cat, index) => (
           <CatCard cat={cat} index={index} key={cat.id} />
         ))}
